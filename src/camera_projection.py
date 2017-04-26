c5a4755e3b67f93e214ca590eba4ad8596606171
Winston Liu <winston.liu@mail.utoronto.ca>
2017-04-26 02:20:30 -0400

    Finished pipeline, still some issues
    Not in Antarctica anymore, that's good.

--- src/camera_projection.py
+++ src/camera_projection.py
@@ -2,6 +2,10 @@
 import numpy
 import math
 
+from transformations import euler_matrix
+
+# x is North-South, y is East-West
+
 # Parameters
 
 max_x = 4096
@@ -22,17 +26,29 @@ K = numpy.matrix(
         [ 0    , f*mxy, Oy , 0],
         [ 0    , 0    , 1  , 0]])
 
-def convert_lat_lon_to_m(d_lat, d_lon):
+DIST_AT_EQUATOR =  111321
+def convert_lat_lon_to_m(d_lat, d_lon, original_lat):
     # Convert lon to m, assuming lat lon in deg, alt in m
     # Using small angle theorem (assume a flat earth)
-    DIST_AT_EQ =  111321
-    lon_m = d_lon * math.cos(lat) * DIST_AT_EQ
-    lat_m = d_lat * DIST_AT_EQ
+    x = d_lat * DIST_AT_EQUATOR
+    y = d_lon * math.cos(original_lat) * DIST_AT_EQUATOR
 
-    return (lon_m, lat_m)
+    return (x, y)
+
+def convert_m_to_lat_lon(x,y, original_lon, original_lat):
+    lat = x / DIST_AT_EQUATOR + original_lat
+    lon = y / (DIST_AT_EQUATOR * math.cos(lat)) + original_lon
+
+    return (lat, lon)
 
 def construct_rotation_matrix(x, y, z, yaw, pitch, roll):
-    pass
+    rotation_mat = euler_matrix(roll, pitch, yaw)
+    # set x,y,z
+    rotation_mat[0][3] = x
+    rotation_mat[1][3] = y
+    rotation_mat[2][3] = z
+
+    return numpy.asmatrix(rotation_mat)
 
 def pixel_to_camera_frame(pixel_x, pixel_y, CAMERA_DIR):
     """ 
@@ -51,8 +67,10 @@ def pixel_to_camera_frame(pixel_x, pixel_y, CAMERA_DIR):
 
     return (camera_x, camera_y)
 
-def world_coordinate_calculation(uv, R):
+def world_coordinate_calculation(xy, R):
     """  
+        xy is in camera frame
+
         Q = K*R
         
         Where K is the 3x4 camera intrinsic matrix, and R is the 4x4 rotation matrix
@@ -65,5 +83,4 @@ def world_coordinate_calculation(uv, R):
 
             Q' x [u,v,1]^T = P
     """
-
-    return (K*R).I * uv
+    return R.I * K.I * xy
--- src/quick_tag.py
+++ src/quick_tag.py
@@ -2,9 +2,10 @@
 import sys, json
 
 from read_logs import *
+from camera_projection import *
 
-# Where forward is in the image
-CAMERA_FWD = "bottom"
+# Which way forward is in the image
+CAMERA_DIR = "bottom"
 
 # 130 in Alma
 ALTITUDE_CORRECTION = 172
@@ -30,4 +31,47 @@ if __name__=="__main__":
     mavlink_datatypes = ["mavlink_global_position_int_t", "mavlink_attitude_t"]
     mavlink_data, mavlink_ref = read_mavlink(sys.argv[2], mavlink_filter, mavlink_datatypes)
 
-    print mavlink_ref
+    output_list = []
+    # Convert relevant parts of the reference reading
+    for key in ["lat", "lon"]:
+        mavlink_ref[key] = float(mavlink_ref[key]) / 1e7
+    mavlink_ref["relative_alt"] = float(mavlink_ref["relative_alt"])
+
+    for i in range(len(mavlink_data["mavlink_global_position_int_t"])):
+        image = annotated_img[i]['filename']
+        # Latitude is degrees * 1e7
+        lat = float(mavlink_data["mavlink_global_position_int_t"][i]["lat"]) / 1e7
+        lon = float(mavlink_data["mavlink_global_position_int_t"][i]["lon"]) / 1e7
+        alt = float(mavlink_data["mavlink_global_position_int_t"][i]["relative_alt"])
+
+        d_lat = lat - mavlink_ref["lat"]
+        d_lon = lon - mavlink_ref["lon"]
+        z = (alt - mavlink_ref["relative_alt"]) / 1e3
+
+        x, y = convert_lat_lon_to_m(d_lat, d_lon, lat)
+
+        roll = float(mavlink_data["mavlink_attitude_t"][i]["roll"])
+        pitch = float(mavlink_data["mavlink_attitude_t"][i]["pitch"])
+        yaw = float(mavlink_data["mavlink_attitude_t"][i]["yaw"])
+
+        R = construct_rotation_matrix(x, y, z, yaw, pitch, roll)
+
+        # Construct pixel information
+
+        for px in annotated_img[i]['annotations']:
+            goosespecies = px['class']
+
+            u = float(px["x"])
+            v = float(px["y"])
+
+            px_x, px_y = pixel_to_camera_frame(u,v, CAMERA_DIR)
+            xy = numpy.matrix([[px_x],[px_y],[1]])
+
+            raw_result = numpy.squeeze(numpy.asarray(world_coordinate_calculation(xy, R)))
+
+            geox = x + raw_result[0]
+            geoy = y + raw_result[1]
+
+            geolat, geolon = convert_m_to_lat_lon(geox, geoy, lat, lon)
+
+            print image, geolon, geolat
